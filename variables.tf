33596046df1a1d68d86646ebd292bbafb7ab6ecc
paladin-devops <83741749+paladin-devops@users.noreply.github.com>
2024-02-14 14:46:09 -0500

    Add a string var.

--- variables.tf
+++ variables.tf
@@ -4,3 +4,8 @@ variable "waypoint_application" {
   description = "Name of the waypoint application, to be used in all resource names."
   type        = string
 }
+
+variable "string_variable" {
+  description = "A string variable."
+  type        = string
+}
