46610e7eaa37e62dc73f2644508798aee7d9b73f
paladin-devops <83741749+paladin-devops@users.noreply.github.com>
2024-09-16 16:46:06 -0400

    Add more variables.

--- variables.tf
+++ variables.tf
@@ -7,3 +7,44 @@ variable "string_variable" {
   description = "A string variable."
   type        = string
 }
+
+variable "string_variable_2" {
+  description = "A second string variable."
+  type        = string
+}
+
+variable "string_variable_3" {
+  description = "A third string variable."
+  type        = string
+}
+
+variable "string_variable_4" {
+  description = "A fourth string variable."
+  type        = string
+}
+
+variable "string_variable_5" {
+  description = "A fifth string variable."
+  type        = string
+}
+
+variable "string_variable_6" {
+  description = "A sixth string variable."
+  type        = string
+}  
+
+variable "string_variable_7" {
+  description = "A seventh string variable."
+  type        = string
+}
+
+variable "string_variable_8" {
+  description = "An eighth string variable."
+  type        = string
+}
+
+variable "string_variable_9" {
+  description = "A ninth string variable."
+  type        = string
+  
+}
