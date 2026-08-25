6a0aa115507432ca2abf93f95d8d1a5938ba7370
paladin-devops <83741749+paladin-devops@users.noreply.github.com>
2026-08-25 16:58:49 -0400

    Add list object variable.

--- variables.tf
+++ variables.tf
@@ -2,3 +2,19 @@ variable "waypoint_application" {
   description = "Name of the waypoint application, to be used in all resource names."
   type        = string
 }
+
+variable "list_object" {
+  description = "A list object variable."
+  type = list(object({
+    a = string
+    b = number
+    c = bool
+    d = list(string)
+    e = list(object({
+      a = string
+      b = number
+      c = bool
+      d = list(string)
+    }))
+  }))
+}
